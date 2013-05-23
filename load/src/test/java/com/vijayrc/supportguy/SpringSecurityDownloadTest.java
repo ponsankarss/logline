43112619a1ea2536ac3459e5938e70c1609fc1e3
vijayrc <vijayrc@outlook.com>
2013-05-23 12:48:47 -0400

    some breakup to spring security test

--- load/src/test/java/com/vijayrc/supportguy/SpringSecurityDownloadTest.java
+++ load/src/test/java/com/vijayrc/supportguy/SpringSecurityDownloadTest.java
@@ -24,47 +24,46 @@ public class SpringSecurityDownloadTest {
     public void shouldDownloadAFileAfterAuthentication() throws Exception {
         DefaultHttpClient httpclient = new DefaultHttpClient();
         try {
-            //1st call   - do a get which is desired 'location'
-            HttpGet httpget = new HttpGet("http://naswasmo01:14010//datanet-lite/export/zip?type=DETAIL&performanceLevelId=8&timeRequested=1360782145531");
+            String location = "http://naswasmo01:14010//datanet-lite/export/zip?type=DETAIL&performanceLevelId=8&timeRequested=1360782145531";
+            authorize(httpclient);
+            HttpGet httpget = new HttpGet(location);
             HttpResponse httpResponse = httpclient.execute(httpget);
-            HttpEntity entity = httpResponse.getEntity();
-            EntityUtils.consume(entity);
-
-            //2nd call  - do a post with credentials get 'location' out
-            HttpPost httppost = new HttpPost("http://naswasmo01:14010/datanet-lite/j_security_check");
-            List <NameValuePair> nvps = new ArrayList<>();
-            nvps.add(new BasicNameValuePair("j_username","t6atchm"));
-            nvps.add(new BasicNameValuePair("j_password", "xxx"));
-            httppost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
-
-            httpResponse = httpclient.execute(httppost);
-
-            Header[] allHeaders = httpResponse.getAllHeaders();
-            String location = "";
-            for (Header header : allHeaders) {
-                if("location".equalsIgnoreCase(header.getName())) location = header.getValue();
-                httpResponse.addHeader(header.getName(), header.getValue());
-            }
-            entity = httpResponse.getEntity();
-            EntityUtils.consume(entity);
-            httpResponse.setStatusCode(httpResponse.getStatusLine().getStatusCode()); // 302
-
-            //3rd call - do a get with the 'location'
-            System.out.println(location);
-            httpget = new HttpGet(location);
-            httpResponse = httpclient.execute(httpget);
-            entity = httpResponse.getEntity();
-
             InputStream content = httpResponse.getEntity().getContent();
-            OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("sometest4.zip")));
+            OutputStream out = new BufferedOutputStream(new FileOutputStream(new File("2.zip")));
 
             IOUtils.copy(content, out);
             out.close();
             content.close();
+            consumeEntity(httpResponse);
 
-//	        EntityUtils.consume(entity);
         } finally {
             httpclient.getConnectionManager().shutdown();
         }
+    }
+
+    private void authorize(DefaultHttpClient httpclient) throws IOException {
+        HttpPost httppost = new HttpPost("http://naswasmo01:14010/datanet-lite/j_security_check");
+        List<NameValuePair> nvps = new ArrayList<>();
+        nvps.add(new BasicNameValuePair("j_username","t6atchm"));
+        nvps.add(new BasicNameValuePair("j_password", "xxx"));
+        httppost.setEntity(new UrlEncodedFormEntity(nvps, HTTP.UTF_8));
+
+        HttpResponse httpResponse = httpclient.execute(httppost);
+        consumeEntity(httpResponse);
+    }
+
+    private void consumeEntity(HttpResponse httpResponse) throws IOException {
+        HttpEntity entity = httpResponse.getEntity();
+        EntityUtils.consume(entity);
+    }
+
+    private String getLocationHeader(HttpResponse httpResponse) {
+        Header[] allHeaders = httpResponse.getAllHeaders();
+        String location = "";
+        for (Header header : allHeaders) {
+            if("location".equalsIgnoreCase(header.getName())) location = header.getValue();
+            httpResponse.addHeader(header.getName(), header.getValue());
+        }
+        return location;
     }
 }
