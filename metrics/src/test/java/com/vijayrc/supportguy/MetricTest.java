3fc62cd8bb6cf10af4dea11f853e01501061b09d
vijayrc <vijayrc@outlook.com>
2013-06-08 13:05:49 -0400

    added metrics test

---
+++ metrics/src/test/java/com/vijayrc/supportguy/MetricTest.java
@@ -0,0 +1,29 @@
+package com.vijayrc.supportguy;
+
+import org.junit.Test;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+public class MetricTest {
+
+    @Test
+    public void shouldUseJMX(){
+        OperatingSystemMXBean operatingSystemMXBean = ManagementFactory.getOperatingSystemMXBean();
+        for (Method method : operatingSystemMXBean.getClass().getDeclaredMethods()) {
+            method.setAccessible(true);
+            if (method.getName().startsWith("get")
+                    && Modifier.isPublic(method.getModifiers())) {
+                Object value;
+                try {
+                    value = method.invoke(operatingSystemMXBean);
+                } catch (Exception e) {
+                    value = e;
+                } // try
+                System.out.println(method.getName() + " = " + value);
+            } // if
+        } // for
+    }
+}
