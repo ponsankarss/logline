97d5c315723068cbdcfbbf82f3d579a5d4f59cbd
vijayrc <vijayrc@outlook.com>
2013-12-09 12:30:29 -0500

    looks like mq conenection  working

--- queue/src/main/java/com/vijayrc/supportguy/queue/queue/domain/QueueManager.java
+++ queue/src/main/java/com/vijayrc/supportguy/queue/queue/domain/QueueManager.java
@@ -1,6 +1,14 @@
 package com.vijayrc.supportguy.queue.queue.domain;
 
 import com.ibm.mq.MQQueueManager;
+import com.ibm.mq.MQC;
+import com.ibm.mq.MQEnvironment;
+import com.ibm.mq.MQException;
+import com.ibm.mq.MQGetMessageOptions;
+import com.ibm.mq.MQMessage;
+import com.ibm.mq.MQPutMessageOptions;
+import com.ibm.mq.MQQueue;
+import com.ibm.mq.MQQueueManager;
 import lombok.Data;
 
 import java.util.List;
@@ -14,7 +22,11 @@ public class QueueManager {
     private List<Queue> queues;
 
     public QueueManager connect() throws Exception{
-        MQQueueManager queueManager = new MQQueueManager(name);
+        MQEnvironment.hostname = "nasmsgdev02";
+        MQEnvironment.port = 1420;
+        MQEnvironment.channel = "NASC.WAS.SVRCONN";
+        MQEnvironment.properties.put(MQC.TRANSPORT_PROPERTY, MQC.TRANSPORT_MQSERIES);
+        MQQueueManager queueManager = new MQQueueManager("MSGDEV02D7");
 
         return this;
     }
